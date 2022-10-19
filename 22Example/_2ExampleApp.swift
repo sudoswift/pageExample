99237f13c725a709c9f1f7d312405222095d97c5
kaito <sudoswift@gmail.com>
2022-10-19 17:22:41 +0900

    Initial Commit

---
+++ 22Example/_2ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2ExampleApp.swift
+//  22Example
+//
+//  Created by ishio on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct _2ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

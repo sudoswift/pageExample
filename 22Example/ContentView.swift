23f7253d7c8d54432ad80054f2f65040a128996d
kaito <sudoswift@gmail.com>
2022-10-19 17:23:46 +0900

    p205 example code

--- 22Example/ContentView.swift
+++ 22Example/ContentView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    var colors: [Color] = [.black, .red, .green, .blue, .cyan]
+    var colornames = ["Black", "Red", "Green", "Blue", "Cyan"]
+    
+    @State private var colorIndex = 0
+    @State private var rotation: Double = 0
+    @State private var welcomeText: String = "WELCOME SWIFTUI !"
+    
+    var body: some View{
+        VStack{
+            Spacer()
+            Text(welcomeText)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(self.rotation))
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: rotation
+                )
+                .foregroundColor(self.colors[self.colorIndex])
+            Spacer()
+            Divider()
+            Picker(selection: $colorIndex, label: Text("Colors")) {
+                ForEach (0 ..< colornames.count, id: \.self){
+                    Text(self.colornames[$0])
+                        .foregroundColor(self.colors[$0])
+                }
+            }
+            .padding()
+            Slider(value: $rotation, in: 0...360, step: 0.1)
+                .padding()
+            
+            TextField("여기에 텍스트를 입력하세요", text: $welcomeText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
         }
         .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
